ec951d2f01aa195eaac8e5904f198234420a1e73
psykomal <bkomalsai@gmail.com>
2023-12-02 19:12:25 +0530

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,19 @@ use std::env;
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+    if let Some(rest) = encoded_value.strip_prefix("i") {
+        if let Some((digit, _)) = rest.split_once("e") {
+            if let Ok(digit) = digit.parse::<i64>() {
+                return digit.into();
+            }
+        }
+    } else if let Some((len, rest)) = encoded_value.split_once(":") {
+        if let Ok(len) = len.parse::<usize>() {
+            return serde_json::Value::String(rest[..len].to_string());
+        }
     }
+
+    panic!("Unhandled encoded value: {}", encoded_value)
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
--- your_bittorrent.sh
+++ your_bittorrent.sh
@@ -6,7 +6,6 @@
 #
 # DON'T EDIT THIS!
 exec cargo run \
-    --quiet \
     --release \
     --target-dir=/tmp/codecrafters-bittorrent-target \
     --manifest-path $(dirname "$0")/Cargo.toml -- "$@"

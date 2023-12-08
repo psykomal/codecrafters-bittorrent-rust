b6c1ad7fe4471deeb310705bb1217f7a32072238
psykomal <bkomalsai@gmail.com>
2023-12-08 17:11:04 +0530

    parse torrent file - fix tests

--- src/main.rs
+++ src/main.rs
@@ -78,9 +78,8 @@ fn main() -> anyhow::Result<()> {
 
     match args.command {
         Command::Decode { value } => {
-            println!("{value}");
-            let v: serde_json::Value = serde_bencode::from_str(&value).unwrap();
-            println!("{v:?}");
+            let v = decode_bencoded_value(&value);
+            println!("{}", v.0);
         }
         Command::Info { torrent } => {
             let dot_torrent = std::fs::read(torrent).context("read torrent file")?;
@@ -134,6 +133,51 @@ mod hashes {
             D: Deserializer<'de>,
         {
             deserializer.deserialize_bytes(HashesVisitor)
+        }
+    }
+}
+
+fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
+    match encoded_value.chars().next() {
+        Some('i') => {
+            let (_, rest) = encoded_value.split_once("i").unwrap();
+            let (n, rest) = rest.split_once("e").unwrap();
+            let n = n.parse::<i64>().unwrap();
+            return (n.into(), rest);
+        }
+        Some('l') => {
+            let (_, mut rest) = encoded_value.split_once("l").unwrap();
+            let mut list = Vec::new();
+            while !rest.is_empty() && !rest.starts_with("e") {
+                let (value, rem) = decode_bencoded_value(rest);
+                list.push(value);
+                rest = rem;
+            }
+            return (list.into(), &rest[1..]);
+        }
+        Some('d') => {
+            let (_, mut rest) = encoded_value.split_once("d").unwrap();
+            let mut dict = serde_json::Map::new();
+            while !rest.is_empty() && !rest.starts_with("e") {
+                let (k, rem) = decode_bencoded_value(rest);
+                let k = match k {
+                    serde_json::Value::String(s) => s,
+                    _ => panic!("Unexpected key type"),
+                };
+
+                let (v, rem) = decode_bencoded_value(rem);
+                dict.insert(k, v);
+                rest = rem;
+            }
+            return (dict.into(), &rest[1..]);
+        }
+        Some('0'..='9') => {
+            let (n, rest) = encoded_value.split_once(":").unwrap();
+            let n = n.parse::<usize>().unwrap();
+            return (serde_json::Value::String(rest[..n].to_string()), &rest[n..]);
+        }
+        _ => {
+            panic!("Unexpected end of encoded value")
         }
     }
 }

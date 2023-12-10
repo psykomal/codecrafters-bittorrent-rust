8e3ea2e0f5c3c93d64b132d3adbf353fb5d24fc6
root <bkomalsai@gmail.com>
2023-12-10 12:45:03 +0000

    download whole file

--- src/main.rs
+++ src/main.rs
@@ -156,7 +156,7 @@ async fn main() -> anyhow::Result<()> {
             }
             let peers_len = peers.len();
             let range = rand::thread_rng().gen_range(0..peers_len);
-            let peer = peers[range];
+            let peer = peers[1];
 
             // Handshake
             let mut peer = tokio::net::TcpStream::connect(peer)
@@ -238,7 +238,7 @@ async fn main() -> anyhow::Result<()> {
             }
             let peers_len = peers.len();
             let range = rand::thread_rng().gen_range(0..peers_len);
-            let peer = peers[range];
+            let peer = peers[1];
 
             // Handshake
             let mut peer = tokio::net::TcpStream::connect(peer)
